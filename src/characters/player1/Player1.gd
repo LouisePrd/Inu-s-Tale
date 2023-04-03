40709edc89bbc520d4fedf587db56e2a62dc4d64
Lwiz <77757761+LouisePrd@users.noreply.github.com>
2023-04-03 10:49:52 +0200

    initial commit

---
+++ src/characters/player1/Player1.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+
+export var speed = 110 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	
+func _physics_process(delta: float) ->  void:
+	var velocity = Vector2.ZERO # The player's movement vector.
+	var is_idling := is_zero_approx(velocity.x) 
+	
+	if Input.is_action_pressed("player1_move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("player1_move_left"):
+		velocity.x -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+
+	$AnimatedSprite.play()
+
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif is_idling:
+		$AnimatedSprite.animation = "idle"
+	else:
+		$AnimatedSprite.animation = "idle"
---
+++ src/screens/Main.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+func _ready():
+#	$MultiTargetCamera2D.add_target($Player)
+#	$MultiTargetCamera2D.add_target($Player2)
+#
+#	$MultiTargetCamera2D.limit_left = 0
+#	$MultiTargetCamera2D.limit_right = get_viewport_rect().size.x
+	pass
