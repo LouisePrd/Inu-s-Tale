bf33f543eaabfa4f1de0afddda89756ceb63f5cb
christelle-bqn <christelle.boquillion@gmail.com>
2023-05-08 23:34:49 +0200

    feat: place assets in entrance scene, add collision at the beginning of the path to stop the players and handle distance between the players

--- src/characters/player1/Player1.gd
+++ src/characters/player1/Player1.gd
@@ -1,25 +1,40 @@
 extends KinematicBody2D
 
+export (NodePath) var player2
+
 export var speed = 200 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
+var can_move = true
 
 func _ready():
 	screen_size = get_viewport_rect().size
-
+	player2 = get_node(player2)
+	
 func _physics_process(delta: float) -> void:
 	var velocity = Vector2.ZERO # The player's movement vector.
 	var is_idling := is_zero_approx(velocity.x) 
-	
+		
 	if Input.is_action_pressed("player1_move_right"):
+		can_move = true
+		if  self.global_position[0] - player2.global_position[0] >= 300:
+			velocity.x = 0
+			$AnimatedSprite.animation = "idle"
+			$AnimatedSprite.play()
+			return
 		velocity.x += 1
 	if Input.is_action_pressed("player1_move_left"):
+		if  self.global_position[0] - player2.global_position[0] <= -300 or can_move == false:
+			velocity.x = 0
+			$AnimatedSprite.animation = "idle"
+			$AnimatedSprite.play()
+			return
 		velocity.x -= 1
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 
 	$AnimatedSprite.play()
-
+		
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	
@@ -32,6 +47,12 @@ func _physics_process(delta: float) -> void:
 	else:
 		$AnimatedSprite.animation = "idle"
 
+func stop():
+	$AnimatedSprite.animation = "idle"
+	can_move = false
 
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("Player"):
+		body.stop()
 
 
