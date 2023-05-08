57de7b839e368590a3b7b39060b808c03f4af27d
Lwiz <77757761+LouisePrd@users.noreply.github.com>
2023-05-08 23:34:17 +0200

    fix dialogue bugs

--- src/screens/dialogues/DialoguePlayer.gd
+++ src/screens/dialogues/DialoguePlayer.gd
@@ -13,6 +13,7 @@ func _ready():
 	
 func play():
 	dialogues = load_dialogue()
+	$NinePatchRect.visible = true
 	current_dialogue_id = -1
 	next_line()
 
@@ -26,8 +27,18 @@ func _input(event):
 func next_line():
 	current_dialogue_id += 1
 	if current_dialogue_id >= len(dialogues):
+		$NinePatchRect.visible = false
+		$"Nao-Sprite".visible = false
+		$"Inu-Sprite".visible = false
 		return
 	dialogues = load_dialogue()
 	$NinePatchRect/Name.text = dialogues[current_dialogue_id]['name']
 	$NinePatchRect/Message.text = dialogues[current_dialogue_id]['text']
+	
+	if dialogues[current_dialogue_id]['name'] == 'Nao':
+		$"Inu-Sprite".visible = false
+		$"Nao-Sprite".visible = true
+	else:
+		$"Inu-Sprite".visible = true
+		$"Nao-Sprite".visible = false
 	
--- src/triggers/firstInteraction.gd
+++ src/triggers/firstInteraction.gd
@@ -1,11 +1,26 @@
 extends Area2D
 
+var active = false
+var launch = false
+var dialogue_player = get_node_or_null("DialoguePlayer")
 
-func _input(event):
-	if event.is_action_pressed("skip") and len(get_overlapping_bodies())>0:
-		find_and_use_dialogue()
-		
-func find_and_use_dialogue():
-	var dialogue_player = get_node_or_null("DialoguePlayers")
+func _ready():
+	connect("body_entered", self, '_on_NPC_body_entered')
+	connect( "body_exited", self, '_on_NPC_body_exited')
 	if dialogue_player:
-		dialogue_player.play()
+		print('existe')
+	else:
+		print('non lol')
+
+func _on_NPC_body_entered(body):
+	if dialogue_player and launch==false:
+		if body. name == 'Player1' or body. name == 'Player2':
+			dialogue_player.play()
+			active = true
+			launch = true
+
+func _on_NPC_body_exited(body):
+	if dialogue_player:
+		if body .name == 'Player1' or body .name == 'Player2':
+			active = false
+
