dba5ee44a45d4d586dd5ac7a21f107e012a05aba
Lwiz <77757761+LouisePrd@users.noreply.github.com>
2023-05-23 14:46:15 +0200

    add dialogue

---
+++ src/screens/cataclysm/SquareTrigger.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+export (NodePath) var player1
+export (NodePath) var player2
+
+var active = false
+var launch = false
+
+func _ready():
+	connect("body_entered", self, '_on_NPC_body_entered')
+	connect( "body_exited", self, '_on_NPC_body_exited')
+	player1 = get_node(player1)
+	player2 = get_node(player2)
+	print('ready')
+
+func _on_NPC_body_entered(body):
+	print('entered')
+	var dialogue_cataclysm = get_node_or_null("DialogueCataclysm")
+	if dialogue_cataclysm and launch==false:
+		if body. name == 'Player1' or body. name == 'Player2':
+			player1.move = false
+			player2.move = false
+			dialogue_cataclysm.play()
+			active = true
+			launch = true
+
+func _on_NPC_body_exited(body):
+	var dialogue_cataclysm = get_node_or_null("DialogueCataclysm")
+	if dialogue_cataclysm:
+		if body .name == 'Player1' or body .name == 'Player2':
+			active = false
