802cb2857922632bf0c0b9095fbcf65fe4ba9f59
christelle-bqn <christelle.boquillion@gmail.com>
2023-06-05 14:55:51 +0200

    feat: update cataclysm sprites and cloud speed

--- src/screens/entrance/CloudLayer.gd
+++ src/screens/entrance/CloudLayer.gd
@@ -1,13 +1,12 @@
 extends ParallaxLayer
 
-export(float) var CLOUD_SPEED = -15.0
-
+export(float) var CLOUD_SPEED = -3.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta) -> void:
 	self.motion_offset.x += CLOUD_SPEED * delta
