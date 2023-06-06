7675ed90a8358a6ade79b4dd4e0a4e2890a743cc
Lwiz <77757761+LouisePrd@users.noreply.github.com>
2023-06-06 10:34:25 +0200

    all interactions cave

---
+++ src/screens/cave/DialogueFrame.gd
@@ -0,0 +1,65 @@
+extends CanvasLayer
+
+export (NodePath) var player1
+export (NodePath) var player2
+
+var dialogue_file = [
+	{"name": "Nao", "text": "Une fresque..."},
+	{"name": "Nao", "text": "Dis donc, on retrouve les mêmes symboles que tout à l’heure..."},
+	{"name": "Inu", "text": "Ce sont des écritures Yokai anciennes..."},
+	{"name": "Inu", "text": "« Le cataclysme signera la suprématie Yokai. Les humains devront se soumettre aux lois des esprits.» "},	
+	{"name": "Nao", "text": "Le cataclysme ? Qu’est-ce que ça veut dire ?"},	
+	{"name": "Nao", "text": "Tu penses que tout ça est un complot, Inu ?"},	
+	{"name": "Inu", "text": "..."},	
+	{"name": "Inu", "text": "Je ne sais pas..."},	
+
+]
+
+var dialogues = []
+var current_dialogue_id = 0
+
+func _ready():
+	$NinePatchRect.visible = false
+	$"Nao-Sprite".visible = false
+	$"Inu-Sprite".visible = false
+
+	player1 = get_node(player1)
+	player2 = get_node(player2)
+	
+func play():
+	dialogues = load_dialogue()
+	$NinePatchRect.visible = true
+	current_dialogue_id = -1
+	next_line()
+
+func load_dialogue():
+	return dialogue_file
+
+func _input(event):
+	if event.is_action_pressed("skip"):
+		next_line()
+		
+func next_line():
+	current_dialogue_id += 1
+	if current_dialogue_id >= len(dialogues):
+		$NinePatchRect.visible = false
+		$"Nao-Sprite".visible = false
+		$"Inu-Sprite".visible = false
+		player1.move = true
+		player2.move = true
+		return
+	dialogues = load_dialogue()
+	$NinePatchRect/Name.text = dialogues[current_dialogue_id]['name']
+	$NinePatchRect/Message.text = dialogues[current_dialogue_id]['text']
+	
+	if dialogues[current_dialogue_id]['name'] == 'Nao':
+		$"Inu-Sprite".visible = false
+		$"Nao-Sprite".visible = true
+	elif dialogues[current_dialogue_id]['name'] == 'Inu':
+		$"Inu-Sprite".visible = true
+		$"Nao-Sprite".visible = false
+	else:
+		$"Inu-Sprite".visible = false
+		$"Nao-Sprite".visible = false
+		
+	
