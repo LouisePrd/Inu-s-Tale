24da15a79ebcf498e07611201e9e0b43c0c3efcb
Lwiz <77757761+LouisePrd@users.noreply.github.com>
2023-06-07 12:43:58 +0200

    error commit src in src

--- src/src/screens/ascent-1/AreaTransition.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-export var scene : PackedScene
-
-func _on_Area2DTransition_body_entered(body) -> void:
-	if body.is_in_group("NPC"):
-		return
-		
-	Transition.transition_to(scene.resource_path)
--- src/src/screens/title_sequence/TitleSequence.gd
+++
@@ -1,9 +0,0 @@
-extends VideoPlayer
-
-export var mainMenu: PackedScene
-
-func _ready():
-	pass
-
-func _on_VideoPlayer_finished():
-	get_tree().change_scene(mainMenu.resource_path)
