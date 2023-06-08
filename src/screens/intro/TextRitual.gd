27955e42612a27809a45c63f372c31fd7c1a7c94
Lwiz <77757761+LouisePrd@users.noreply.github.com>
2023-06-08 14:33:29 +0200

    update : intro

--- src/screens/intro/TextRitual.gd
+++ src/screens/intro/TextRitual.gd
@@ -2,12 +2,19 @@ extends RichTextLabel
 
 
 export (NodePath) var TextRitual2
+export (NodePath) var inugami
+export (NodePath) var boom
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	TextRitual2 = get_node(TextRitual2)
+	inugami = get_node(inugami)
+	boom = get_node(boom)
 	TextRitual2.visible = false
+	inugami.visible = false
+	boom.visible = false
 	pass # Replace with function body.
 
 func _input(event):
@@ -15,6 +22,10 @@ func _input(event):
 		if self.visible == true:
 			self.visible = false
 			TextRitual2.visible = true
+			TextRitual2.visible = true
+			boom.visible=true
 		elif TextRitual2.visible == true:
 			TextRitual2.visible = false
+			inugami.visible = true
+			boom.visible= false
 			TextRitual2.launchTimer()
