bbfc0c08b4f06ed37db546f80ef593a3906a2803
christelle-bqn <christelle.boquillion@gmail.com>
2023-06-08 14:35:49 +0200

    feat: handle autoload intro music

--- src/screens/title_sequence/TitleSequence.gd
+++ src/screens/title_sequence/TitleSequence.gd
@@ -3,7 +3,8 @@ extends VideoPlayer
 export var mainMenu: PackedScene
 
 func _ready():
-	pass
+	var autoload = $"/root/AutoloadMusic".get_children()
+	autoload[0].stop()
 
 func _on_VideoPlayer_finished():
 	get_tree().change_scene(mainMenu.resource_path)
