7c2f3057724c2f290d6651f19ef91b9d8cdd2734
Lwiz <77757761+LouisePrd@users.noreply.github.com>
2023-06-08 13:50:45 +0200

    update intro + music

--- src/screens/intro/TextRitual.gd
+++ src/screens/intro/TextRitual.gd
@@ -1,12 +1,20 @@
 extends RichTextLabel
 
-export var scene : PackedScene
+
+export (NodePath) var TextRitual2
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	TextRitual2 = get_node(TextRitual2)
+	TextRitual2.visible = false
 	pass # Replace with function body.
 
-
 func _input(event):
-	pass
+	if event.is_action_pressed("Enter"):
+		if self.visible == true:
+			self.visible = false
+			TextRitual2.visible = true
+		elif TextRitual2.visible == true:
+			TextRitual2.visible = false
+			TextRitual2.launchTimer()
